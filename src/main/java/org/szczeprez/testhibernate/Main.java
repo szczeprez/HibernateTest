3ea93c4196ab2039f5247ca3f4a3f0d8613dd235
Piotr <szczeprez@poczta.fm>
2016-06-25 19:53:42 +0200

    dodana adnotacja Transient oraz typy danych do bazy

--- src/main/java/org/szczeprez/testhibernate/Main.java
+++ src/main/java/org/szczeprez/testhibernate/Main.java
@@ -1,5 +1,7 @@
 package org.szczeprez.testhibernate;
 
+import java.math.BigDecimal;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -9,29 +11,21 @@ import org.szczeprez.testhibernate.domain.Employee;
 public class Main {
 
 	public static void main(String[] args) {
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myDatabase"); 
-		EntityManager entityManager = entityManagerFactory.createEntityManager(); 
-		
-		Employee employee = new Employee(); 
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myDatabase");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+/*		Employee employee = new Employee();
 		employee.setFirstName("Jan");
 		employee.setLastName("Kowalski");
 		employee.setSalary(3333.333);
-		
-		Employee employee2 = new Employee(); 
-		employee2.setFirstName("Micha³");
-		employee2.setLastName("Nowak");
-		employee2.setSalary(2222.22);
-		
+		employee.setSalary2(new BigDecimal("12345.3"));
 		
 		entityManager.getTransaction().begin();
 		entityManager.persist(employee);
-		entityManager.persist(employee2);
-		entityManager.getTransaction().commit();
-		
-		
+		entityManager.getTransaction().commit();*/
+
 		entityManager.close();
 		entityManagerFactory.close();
 
 	}
-
 }
