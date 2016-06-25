c7673f796ed135629a4e24e8101be73d84345e7d
Piotr <szczeprez@poczta.fm>
2016-06-25 19:31:00 +0200

    dodany employee

--- src/main/java/org/szczeprez/testhibernate/Main.java
+++ src/main/java/org/szczeprez/testhibernate/Main.java
@@ -4,11 +4,30 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.szczeprez.testhibernate.domain.Employee;
+
 public class Main {
 
 	public static void main(String[] args) {
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myDatabase"); 
 		EntityManager entityManager = entityManagerFactory.createEntityManager(); 
+		
+		Employee employee = new Employee(); 
+		employee.setFirstName("Jan");
+		employee.setLastName("Kowalski");
+		employee.setSalary(3333.333);
+		
+		Employee employee2 = new Employee(); 
+		employee2.setFirstName("Micha³");
+		employee2.setLastName("Nowak");
+		employee2.setSalary(2222.22);
+		
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(employee);
+		entityManager.persist(employee2);
+		entityManager.getTransaction().commit();
+		
 		
 		entityManager.close();
 		entityManagerFactory.close();
