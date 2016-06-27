dce3288f5b868e30248beb7d83df6ab9595200cf
Piotr <szczeprez@poczta.fm>
2016-06-27 20:18:52 +0200

    relacja one-to-one

--- src/main/java/org/szczeprez/testhibernate/Main.java
+++ src/main/java/org/szczeprez/testhibernate/Main.java
@@ -7,6 +7,8 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.szczeprez.testhibernate.domain.Employee;
+import org.szczeprez.testhibernate.domain1.Employee1;
+import org.szczeprez.testhibernate.domain1.Employee2;
 
 public class Main {
 
@@ -14,15 +16,21 @@ public class Main {
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myDatabase");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-/*		Employee employee = new Employee();
+		Employee2 employee2 = new Employee2(); 
+		
+/*		Employee1 employee = new Employee1();
 		employee.setFirstName("Jan");
 		employee.setLastName("Kowalski");
 		employee.setSalary(3333.333);
-		employee.setSalary2(new BigDecimal("12345.3"));
+		employee.setLocality("Warszawa");
+		employee.setZipCode("11-111");
+		employee.setStreet("Koszykowa"); 
+		employee.setStreetNumber(111);*/
+ 
 		
 		entityManager.getTransaction().begin();
-		entityManager.persist(employee);
-		entityManager.getTransaction().commit();*/
+		entityManager.persist(employee2);
+		entityManager.getTransaction().commit();
 
 		entityManager.close();
 		entityManagerFactory.close();
