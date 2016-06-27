e549e6541907cda82ce3a51d39b6bdb58f774f72
Piotr <szczeprez@poczta.fm>
2016-06-27 20:44:12 +0200

    next commit master

--- src/main/java/org/szczeprez/testhibernate/Main.java
+++ src/main/java/org/szczeprez/testhibernate/Main.java
@@ -4,12 +4,21 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.szczeprez.testhibernate.domain1.Employee3;
+
 public class Main {
 
 	public static void main(String[] args) {
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myDatabase"); 
 		EntityManager entityManager = entityManagerFactory.createEntityManager(); 
+		Employee3 employee3 = new Employee3();
+		employee3.setFirstName("Jan");
+		employee3.setLastName("Nowak");
+		employee3.setSalary(1000.0);
 		
+		entityManager.getTransaction().begin();
+		entityManager.persist(employee3);
+		entityManager.getTransaction().commit();
 		entityManager.close();
 		entityManagerFactory.close();
 
